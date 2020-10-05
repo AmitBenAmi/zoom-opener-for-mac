7059430fe083dceb4a3942619d387f20600b4729
Amit Ben Ami <benami.amit1@gmail.com>
2020-10-05 19:13:33 +0300

    Adding colors for all echos

--- create-app.sh
+++ create-app.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+GREEN='\033[1;32m'
+NC='\033[0m'
+
 if [[ $# -ne 4 ]];
 then
-    YELLOW="\033[1;33m"
-    WHITE="\033[1;37m"
-    NC="\033[0m"
-    echo -e "${YELLOW}Not a valid command, not enough parameters passed. Usage:"
-    echo -e "${WHITE}./create-app.sh \"app-name\" bundle-identifier zoom-url \"/path/to/icon.icns\""
-    echo -e "\n${YELLOW}For Example:"
-    echo -e "${WHITE}./create-app.sh \"Open Zoom\" zoom.open.my-company.com https://us04web.zoom.us/j/77950543638 \"~/my-icongs/zoom-icon.icns\"${NC}"
+    YELLOW='\033[1;33m'
+    WHITE='\033[1;37m'
+    echo -e "${YELLOW}"'Not a valid command, not enough parameters passed. Usage:'
+    echo -e "${WHITE}"'./create-app.sh "app-name" "bundle-identifier" "zoom-url" "/path/to/icon.icns"'
+    echo -e '\n'"${YELLOW}"'For Example:'
+    echo -e "${WHITE}"'./create-app.sh "Open Zoom" "zoom.open.my-company.com" "https://us04web.zoom.us/j/77950543638" "~/my-icongs/zoom-icon.icns"'"${NC}"
     exit 1
 fi
 
@@ -17,28 +19,28 @@ BUNDLE_IDENTIFIER=$2
 ZOOM_URL=$(echo $3 | sed -E 's/(http|https):\/\///')
 ICON_LOCATION=$4
 
-echo "Changing the startup script"
+echo "${GREEN}"'Changing the startup script'"${NC}"
 cp ./Contents/MacOS/app.sh ./app.sh.backup
 sed -i 's/<url-for-zoom>/$ZOOM_URL/' ./Contents/MacOS/app.sh
 mv ./Contents/MacOS/app.sh ./Contents/MacOS/$APP_NAME.sh
 
-echo "Changing the icon"
+echo "${GREEN}"'Changing the icon'"${NC}"
 mv ./Contents/Resources/icon.icns ./icon.icns.backup
 mv $ICON_LOCATION ./Contents/Resources/icon.icns
 
-echo "Changing the Info.plist"
+echo "${GREEN}"'Changing the Info.plist'"${NC}"
 cp ./Contents/Info.plist ./Info.plist.backup
 sed -i 's/<AppName>/$APP_NAME/g' ./Contents/Info.plist
 sed -i 's/<CFBundleIdentifier>/$BUNDLE_IDENTIFIER/' ./Contents/Info.plist
 
-echo "Packaging the app"
+echo "${GREEN}"'Packaging the app'"${NC}"
 pkgbuild \
     --install-location /Applications/$APP_NAME.app \
     --root .  \
     --identifier $BUNDLE_IDENTIFIER \
     ./$APP_NAME.pkg
 
-echo "Reverting all of the changes"
+echo "${GREEN}"'Reverting all of the changes'"${NC}"
 rm ./Contents/MacOS/$APP_NAME.sh
 mv ./app.sh.backup ./Contents/MacOS/app.sh
 mv ./icon.icns.backup ./Contents/Resources/icon.icns
